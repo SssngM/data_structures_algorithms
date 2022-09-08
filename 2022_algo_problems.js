d80d15c3036ffb45a9a9a8d24e1aa6fb90bc176a
SssngM <rarang222@gmail.com>
2022-09-07 19:44:30 -0700

    update

--- 2022_algo_problems.js
+++ 2022_algo_problems.js
@@ -826,7 +826,7 @@ function solution(inputString) {
 
 
 
-// alphabeticShift
+// alphabeticShift (codesignal)
 //Given a string, your task is to replace each of its characters by the next one in the English alphabet; 
 //i.e. replace a with b, replace b with c, etc (z would be replaced by a).
 
