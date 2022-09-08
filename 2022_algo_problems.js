1646c1eb302877a22a3905d064e0d9538087c91f
SssngM <rarang222@gmail.com>
2022-09-07 19:47:11 -0700

    update

--- 2022_algo_problems.js
+++ 2022_algo_problems.js
@@ -805,9 +805,11 @@ function sortByheight(a) {
 
 
 
+// alphabeticShift (codesignal)
+//Given a string, your task is to replace each of its characters by the next one in the English alphabet; 
+//i.e. replace a with b, replace b with c, etc (z would be replaced by a).
 
 // Given a string, your task is to replace each of its characters by the next one in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
-
 // For inputString = "crazy", the output should be solution(inputString) = "dsbaz".
 
 
@@ -826,23 +828,9 @@ function solution(inputString) {
 
 
 
-// alphabeticShift (codesignal)
-//Given a string, your task is to replace each of its characters by the next one in the English alphabet; 
-//i.e. replace a with b, replace b with c, etc (z would be replaced by a).
 
 
-function solution(inputString) {
-  let newStr = ''
-  let alpha = " abcdefghijklmnopqrstuvwxyza"
-  for (let cur of inputString) {
-      
-      let idx = alpha.indexOf(cur) // crazy 
-      idx = idx+1
-      newStr += alpha[idx]
-      console.log(alpha[idx])
-  } 
-  return newStr
-}
+
 
 
 
