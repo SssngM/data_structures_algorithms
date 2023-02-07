020e27bd239a8320b68616b709aec1065c444da9
SssngM <rarang222@gmail.com>
2023-02-07 11:19:01 -0800

    practice

---
+++ app_practice.js
@@ -0,0 +1,24 @@
+// Write a function evenRange(start, end) that returns an array containing all even
+// numbers between 'start' and 'end' in sequential order.
+
+evenRange(13, 20) //=> [ 14, 16, 18, 20 ]
+evenRange(4, 11) //=> [ 4, 6, 8, 10 ]
+evenRange(8, 5) //=> []
+
+
+function evenRange(start, end) {
+
+    
+
+}
+
+
+//Write a function reverseString(string) that takes in a hyphenated string and
+//returns a the hyphenated string reversed.
+
+reverseString("Go-to-the-store") // => "store-the-to-Go"
+reverseString("Jump,-jump-for-joy")  // => "joy-for-jump-Jump,"
+
+function reverseString(string) {
+  // your code here...
+}
